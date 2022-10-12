558ba15a2b46a70bacb6397d2259e2eeed90b366
Saikat Dey <30950554+deysaikat04@users.noreply.github.com>
2022-10-12 10:56:47 +0530

    chore: added validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,25 @@
-document.getElementById("btn").addEventListener("click", openURL);
+var buttonElement = document.getElementById("btn");
+buttonElement.addEventListener("click", openURL);
+
+var inputElement = document.getElementById("ticketId");
+inputElement.addEventListener("keyup", validate);
 
 function openURL(e) {
-    e.preventDefault();
-    var baseURL = "https://dev.azure.com/PropertyLoop/PropertyLoop/_workitems/edit/";
-    let inputValue = document.getElementById("ticketId").value;
+  e.preventDefault();
+  //   add your base URL here
+  let baseURL = "";
+  chrome.tabs.create({ url: baseURL + inputElement.value });
+}
 
-    chrome.tabs.create({ url: baseURL+inputValue });
+function validate() {
+  if (inputElement.value.length == 0) {
+    inputElement.classList.remove("input_error");
+    buttonElement.disabled = true;
+  } else if (/^[0-9]{3,7}$/.test(inputElement.value)) {
+    inputElement.classList.remove("input_error");
+    buttonElement.disabled = false;
+  } else {
+    inputElement.classList.add("input_error");
+    buttonElement.disabled = true;
+  }
 }
